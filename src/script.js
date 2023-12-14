262e578d8fa8b76ca239e00e10f42180ea493fff
RebeccaCL303 <126423309+RebeccaCL303@users.noreply.github.com>
2023-12-14 13:36:25 -0600

    added genre to prompt, fixed bugs
    fixed errors:  js only recognized prompt as a function, changed name to input. Moved url to handleSubmit function so it could get the updated url with form parameters

--- src/script.js
+++ src/script.js
@@ -1,29 +1,39 @@
 let mediaType = "";
-let prompt = "";
+let genre = "";
+let input = "";
 
 const key = "82f43b0671f2tb328187o7be4ab620aa";
 const context = "Make sure to provide a clear and prescise answer.";
-const url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
+const form = document.querySelector("form");
+const mediaElement = document.querySelector("#media-type");
+const genreElement = document.querySelector("#genre");
 const description = document.querySelector(".description");
+
+// handle input change
 
 function handleChange() {
  mediaType = mediaElement.value;
- prompt = `What is a good ${mediaType} to watch? Please limit the answer to one item, and provide a brief spoiler free summary of the ${mediaType}.`;
- console.log(prompt);
+ genre = genreElement.value;
+ input = `What is a good ${genre} ${mediaType} to watch? Please limit the answer to one item, and provide a brief spoiler free summary of the ${mediaType}.`;
+ console.log(input);
 }
 
-const mediaElement = document.querySelector("#media-type");
 mediaElement.addEventListener("change", handleChange);
+genreElement.addEventListener("change", handleChange);
+
+// handle submit
 
 function handleSubmit(event) {
  event.preventDefault();
  description.innerHTML = "Loading...";
+ const url = `https://api.shecodes.io/ai/v1/generate?prompt=${input}&context=${context}&key=${key}`;
  axios.get(url).then(test);
 }
 
-const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
+
+// response
 
 function test(response) {
  console.log(response);
